910ff46f38e131c12b3f594045aa2159049a7cb8
João da Rocha <joaorocha@gmail.com>
2020-09-15 17:11:09 +0100

    Create README.m
    Created README file

---
+++ README.m
@@ -0,0 +1,3 @@
+Work in Progress:
+This is were I will store my hacks, like shell scripts that my life easier and automate some of the boring repetitive tasks, 
+and also my config files for my shell or the text editors that I use.
