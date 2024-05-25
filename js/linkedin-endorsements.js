ff2081917f6e3be37e6edae3152bea0549e9c4e4
João Rocha <joaorocha@gmail.com>
2024-05-25 11:08:50 +0100

    feat: add script to automate Linkedin endorsements

---
+++ js/linkedin-endorsements.js
@@ -0,0 +1,37 @@
+let interval = null;
+let skills = null;
+const endorseBtnSelector = "button.artdeco-button .artdeco-button__text";
+const loadMoreBtnSelector = ".scaffold-finite-scroll__load-button";
+const getSkillsToEndorse = () => {
+    return Array.from(document.querySelectorAll(endorseBtnSelector)).filter(
+        (e) => e.innerText === "Endorse"
+    );
+};
+
+const stop = () => {
+    clearInterval(interval);
+    console.log("Done!");
+};
+const run = () => {
+    interval = setInterval(() => {
+        let skills = getSkillsToEndorse();
+        console.log("Ok, let's do this!");
+        const loadMoreBtn = document.querySelector(loadMoreBtnSelector);
+        if (loadMoreBtn && !skills.length) {
+            console.log("Scrolling to bottom..");
+            window.scrollTo(0, document.body.scrollHeight);
+            setTimeout(() => {
+                clearInterval(interval);
+                run();
+            }, 2000);
+        } else {
+            console.log(skills);
+            if (!skills.length) {
+                stop();
+            } else {
+                skills[0].click();
+            }
+        }
+    }, 5000);
+};
+run();
